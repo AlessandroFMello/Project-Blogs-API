5bfce1f47b33c439fba5ba57d39edb827d889560
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-03-11 03:21:30 -0300

    chore: login validation middlewares indexing

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,7 +1,11 @@
 const errorMiddleware = require('./errorMiddleware');
 const userValidation = require('./userValidationMiddleware');
+const loginValidation = require('./loginValidationMidleware');
+const emailPassValidation = require('./emailPassValidation');
 
 module.exports = {
   errorMiddleware,
   userValidation,
+  loginValidation,
+  emailPassValidation,
 };
