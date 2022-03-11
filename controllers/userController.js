eed7baef6bfff4a9588a126a79e357cd5aed09e6
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-03-11 01:36:15 -0300

    feat: creates user controller

---
+++ controllers/userController.js
@@ -0,0 +1,18 @@
+const { userService } = require('../services');
+
+module.exports = {
+  create: async (req, res, _next) => {
+    try {
+      const { code, message, user } = await userService.create(req.body);
+  
+      if (!user) {
+        return res.status(code).json({ message });
+      }
+
+      return res.status(code).json(user);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: 'Algo deu errado' });
+    }
+  },
+};
