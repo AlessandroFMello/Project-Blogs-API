66de576d4267e783463a1f95480e469ea9748364
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-03-11 19:05:22 -0300

    fix: removes unused indexing

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,13 +1,11 @@
 const errorMiddleware = require('./errorMiddleware');
 const userValidation = require('./userValidationMiddleware');
 const loginValidation = require('./loginValidationMidleware');
-const emailPassValidation = require('./emailPassValidation');
 const tokenValidation = require('./tokenValidation');
 
 module.exports = {
   errorMiddleware,
   userValidation,
   loginValidation,
-  emailPassValidation,
   tokenValidation,
 };
