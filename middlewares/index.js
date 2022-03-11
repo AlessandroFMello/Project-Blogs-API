7bff620e5b1db602b102fbb5c68ede13f73e78c4
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-03-11 13:09:10 -0300

    chore: token validation middleware indexing

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,10 +2,12 @@ const errorMiddleware = require('./errorMiddleware');
 const userValidation = require('./userValidationMiddleware');
 const loginValidation = require('./loginValidationMidleware');
 const emailPassValidation = require('./emailPassValidation');
+const tokenValidation = require('./tokenValidation');
 
 module.exports = {
   errorMiddleware,
   userValidation,
   loginValidation,
   emailPassValidation,
+  tokenValidation,
 };
