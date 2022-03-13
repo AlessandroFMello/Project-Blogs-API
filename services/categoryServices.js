cd403f1037d1f8c886feca1fe5495463a19cdd4a
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-03-13 00:09:32 -0300

    chore: creates categories services layer

---
+++ services/categoryServices.js
@@ -0,0 +1,15 @@
+const { Category } = require('../models');
+
+module.exports = {
+  create: async ({ name }) => {
+    const checkIfCategoryAlreadyExists = await Category.findOne({ where: { name } });
+
+    if (checkIfCategoryAlreadyExists) {
+      return { code: 409, message: 'Category already registered' };
+    }
+
+    const newCategory = await Category.create({ name });
+
+    return { code: 201, category: newCategory };
+  },
+};
