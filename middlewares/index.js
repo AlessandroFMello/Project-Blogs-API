932b7d8e65aa67de2191879bf51f1ebf94b61d05
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-03-13 00:11:13 -0300

    chore: category validadion middleware indexing

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,6 +1,7 @@
 const errorMiddleware = require('./errorMiddleware');
 const userValidation = require('./userValidationMiddleware');
 const loginValidation = require('./loginValidationMidleware');
+const categoryValidation = require('./categoryValidation');
 const tokenValidation = require('./tokenValidation');
 
 module.exports = {
@@ -8,4 +9,5 @@ module.exports = {
   userValidation,
   loginValidation,
   tokenValidation,
+  categoryValidation,
 };
