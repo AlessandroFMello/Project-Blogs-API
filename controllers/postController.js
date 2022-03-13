2cd8eff5b38aa29d14cee5d22bfd407bce7b8555
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-03-13 11:07:09 -0300

    fix: changes error message

--- controllers/postController.js
+++ controllers/postController.js
@@ -13,7 +13,9 @@ module.exports = {
       return res.status(code).json(post);
     } catch (error) {
       console.log(error);
-      return res.status(500).json({ message: 'Algo deu errado' });
+      return res.status(500).json({ message: error.message });
+    }
+  },
     }
   },
 };
