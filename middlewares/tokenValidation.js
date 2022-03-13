da4c4629c81f28b5116d070edfb769444553afa9
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-03-13 09:57:51 -0300

    fix: changes token validation variable name

--- middlewares/tokenValidation.js
+++ middlewares/tokenValidation.js
@@ -5,13 +5,12 @@ const secret = process.env.JWT_SECRET;
 
 module.exports = (req, res, next) => {
   const token = req.headers.authorization;
-  console.log(token);
   if (!token) {
     return res.status(401).json({ message: 'Token not found' });
   }
   try {
     const validatesUsers = jwt.verify(token, secret);
-    req.user = validatesUsers;
+    req.token = validatesUsers;
     next();
   } catch (error) {
     console.log(error);
