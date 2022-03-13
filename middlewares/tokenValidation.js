f75022a6e540c51c284e019d644220ec2975530a
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-03-12 21:18:38 -0300

    fix: changes the environment variable name

--- middlewares/tokenValidation.js
+++ middlewares/tokenValidation.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
-const secret = process.env.SECRET;
+const secret = process.env.JWT_SECRET;
 
 module.exports = (req, res, next) => {
   const token = req.headers.authorization;
