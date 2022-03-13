039b9cc4725e6f95036b8915faabde7081d2f4c4
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-03-13 00:09:42 -0300

    chore: category services indexing

--- services/index.js
+++ services/index.js
@@ -1,7 +1,9 @@
 const userService = require('./userServices');
 const loginService = require('./loginServices');
+const categoryService = require('./categoryServices');
 
 module.exports = {
   userService,
   loginService,
+  categoryService,
 };
