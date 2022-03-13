edd0225cdbc29569ae7957e1ed7377c2ef860902
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-03-13 09:59:03 -0300

    feat: post services indexing

--- services/index.js
+++ services/index.js
@@ -1,9 +1,11 @@
 const userService = require('./userServices');
 const loginService = require('./loginServices');
 const categoryService = require('./categoryServices');
+const postService = require('./postServices');
 
 module.exports = {
   userService,
   loginService,
   categoryService,
+  postService,
 };
