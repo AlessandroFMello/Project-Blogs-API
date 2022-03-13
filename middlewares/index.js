ed7ac7190e7dae58aae211705754c7730862d14e
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-03-13 09:58:35 -0300

    const post validation middleware indexing

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,6 +2,7 @@ const errorMiddleware = require('./errorMiddleware');
 const userValidation = require('./userValidationMiddleware');
 const loginValidation = require('./loginValidationMidleware');
 const categoryValidation = require('./categoryValidation');
+const postValidation = require('./postValidationMiddleware');
 const tokenValidation = require('./tokenValidation');
 
 module.exports = {
@@ -10,4 +11,5 @@ module.exports = {
   loginValidation,
   tokenValidation,
   categoryValidation,
+  postValidation,
 };
